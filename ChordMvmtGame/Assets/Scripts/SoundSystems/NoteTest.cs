e4809afa12b6234dddd3ff457c3e5bf413e2f5e8
BOOG\Jenny <jfullert@ucsc.edu>
2018-07-18 17:14:24 -0700

    SOUND IS WORKING

--- ChordMvmtGame/Assets/Scripts/SoundSystems/NoteTest.cs
+++ ChordMvmtGame/Assets/Scripts/SoundSystems/NoteTest.cs
@@ -5,21 +5,17 @@ public class NoteTest : MonoBehaviour {
 	// stolen directly from aldonaletto
 	// < https://answers.unity.com/questions/141771/whats-a-good-way-to-do-dynamically-generated-music.html?points=1 >
 
-	//float tranpose = -4;	// transpose in semitones
-		// do i know what a semitone is? NO!
-		// but that's fine
-
 	// component used by unity to play the clip
+	// basically a reference to itsel but okay
+	// i know how to program amirite fellas
 	public AudioSource musicSource;
 
-	void Start () {
-		
-	}
-	
+	float tranpose = -4;	// transpose in semitones
+	// i know exactly what the fuck i'm doing
 
 	void Update () {
-		/*
-		float note = -1f; // ...invalid note
+		
+		float note = -1f;
 
 		if (Input.GetKeyDown("a")) note = 0f;  // C
 		if (Input.GetKeyDown("s")) note = 2f;  // D
@@ -34,12 +30,16 @@ public class NoteTest : MonoBehaviour {
 		// if something is pressed
 		if (note >= 0)
 		{ 
-			
+			// I. DON'T. KNOW. HOW. THIS. WORKS. OR. WHAT. IT.
+			//	MEANS. BUT. IT. WORKS.   B I T C H !
+			musicSource.pitch = Mathf.Pow (2, (note+tranpose)/12f);
+			musicSource.Play ();
 		}
-		*/
 
+		/*
 		// test
 		if (Input.GetKeyDown (KeyCode.Space))
 			musicSource.Play ();
+		*/
 	}
 }
